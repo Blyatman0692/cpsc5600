5c676bd0f1051966242b7328f3dd68e00b134937
Blyatman0692 <jzheng2@seattleu.edu>
2026-01-12 21:41:24 -0800

    init hw1

---
+++ homework/hw1/hw1.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Junwen Zheng on 1/11/26.
+//
+
+#include <vector>
+const int N = 1<<26;  // FIXME must be power of 2 for now
+
+typedef std::vector<int> Data;
+
+int main() {
+    Data data(N, 1);  // put a 1 in each element of the data array
+    data[0] = 10;
+    Data prefix(N, 1);
+
+    // start timer
+    auto start = chrono::steady_clock::now();
+
+    SumHeap heap(&data);
+    heap.prefixSums(&prefix);
+
+    // stop timer
+    auto end = chrono::steady_clock::now();
+    auto elpased = chrono::duration<double,milli>(end-start).count();
+
+    int check = 10;
+    for (int elem: prefix)
+        if (elem != check++) {
+            cout << "FAILED RESULT at " << check-1;
+            break;
+        }
+    cout << "in " << elpased << "ms" << endl;
+    return 0;
+}
