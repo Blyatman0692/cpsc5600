a42f6893c301a33b1c7e9eb9bae4ecf2fb8e6536
Blyatman0692 <jzheng2@seattleu.edu>
2026-01-14 21:05:21 -0800

    implemented Heaper base class and SumHeap class

--- homework/hw1/hw1.cpp
+++ homework/hw1/hw1.cpp
@@ -2,10 +2,88 @@
 // Created by Junwen Zheng on 1/11/26.
 //
 
+#include <future>
 #include <vector>
+#include <iostream>
+using namespace std;
 const int N = 1<<26;  // FIXME must be power of 2 for now
 
-typedef std::vector<int> Data;
+typedef vector<int> Data;
+
+class Heaper {
+public:
+    Heaper(const Data* data): n(static_cast<int>(data->size())), data(data) {
+        interior = new Data(n - 1, 0);
+    }
+
+    ~Heaper() {
+        delete[] interior;
+    }
+
+protected:
+    int n;
+    const Data* data;
+    Data* interior;
+
+    virtual int size() {
+        return n - 1 + n;
+    }
+
+    virtual int value(int i) {
+        if (i < n - 1) {
+            return interior->at(i);
+        }
+        return data->at(i - (n - 1));
+    }
+
+    virtual int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    virtual int left(int i) {
+        return (2 * i) + 1;
+    }
+
+    virtual int right(int i) {
+        return (2 * i) + 2;
+    }
+
+    virtual bool isLeaf(int i) {
+        if (i < n - 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+};
+
+class SumHeap: public Heaper {
+public:
+    SumHeap(const Data* data): Heaper(data) {
+        calcSum(0);
+    }
+
+    void prefixSums(Data* output) {
+
+    }
+
+private:
+    void calcSum(int i) {
+        // base case
+        if (isLeaf(i)) {
+            return;
+        }
+
+        calcSum(left(i));
+        calcSum(right(i));
+
+        interior->at(i) = value(left(i)) + value(right(i));
+    }
+
+
+
+};
 
 int main() {
     Data data(N, 1);  // put a 1 in each element of the data array
