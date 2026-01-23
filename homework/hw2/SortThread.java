b66970c2b92ccfb3a0d5a623729297a3cc861fdb
Blyatman0692 <jzheng2@seattleu.edu>
2026-01-23 10:21:12 -0800

    implemented single thread bitonic sort loop version

---
+++ homework/hw2/SortThread.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+class SortThread implements Runnable {
+    public SortThread(int p, int n) {
+        this.n = n;
+        data = new int[n];
+    }
+
+    public void run() {
+        // block size
+        for (int k = 2; k <= n; k *= 2) {
+            // distance between the two elements being compared
+            for (int j = k / 2; j > 0; j /= 2) {
+                // iterate through the element inside the block
+                for (int i = 0; i < n; i++) {
+
+                    // partner index
+                    int ixj = i ^ j;
+
+                    if (ixj > i) {
+                        if ((i & k) == 0 && data[i] > data[ixj]) {
+                            swap(data[i], data[ixj]);
+                        }
+                        if ((i & k) != 0 && data[i] < data[ixj]) {
+                            swap(data[i], data[ixj]);
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private int [] data;
+    private final int n;
+
+    private void swap(int i, int j) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+}
