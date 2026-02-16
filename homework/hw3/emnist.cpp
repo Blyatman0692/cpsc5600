7f4cb8dfda34f55034ef0d22f66027a7e862025e
Blyatman0692 <jzheng2@seattleu.edu>
2026-02-15 18:30:01 -0800

    added a simple section to show converged results

--- homework/hw3/emnist.cpp
+++ homework/hw3/emnist.cpp
@@ -5,7 +5,8 @@
 #include "IdxIO.h"
 #include "EMNISTKMeansMPI.h"
 
-const int K = 10;
+// K should be fixed to 10 since we have digits 0..9
+constexpr int K = 10;
 
 int main(int argc, char** argv) {
     if (argc < 3) {
@@ -25,7 +26,48 @@ int main(int argc, char** argv) {
         auto images = std::move(imgs.images);
 
         emnist.fit(images.data(), images.size());
-        std::cout << labels[0] << std::endl;
+
+        // report converged results
+        const auto &clusters = emnist.getClusters();
+
+        int correct = 0;
+        int total = 0;
+
+        for (int c = 0; c < K; c++) {
+            std::array<int, 10> freq{};
+            for (int idx : clusters[c].elements) {
+                if (idx >= 0 && idx < static_cast<int>(labels.size())) {
+                    int d = static_cast<int>(labels[idx]);
+                    if (0 <= d && d < 10) {
+                        freq[d]++;
+                    }
+                }
+            }
+
+            const int maj = static_cast<int>(std::distance(freq.begin(), std::max_element(freq.begin(), freq.end())));
+            std::cout << "Cluster " << c
+                      << ": size = " << clusters[c].elements.size()
+                      << ", majority digit = " << maj
+                      << std::endl;
+
+            // contribute to accuracy
+            for (int idx : clusters[c].elements) {
+                if (idx >= 0 && idx < static_cast<int>(labels.size())) {
+                    total++;
+                    if (static_cast<int>(labels[idx]) == maj) {
+                        correct++;
+                    }
+                }
+            }
+        }
+
+        if (total > 0) {
+            std::cout << "Accuracy: "
+                      << (100.0 * static_cast<double>(correct) / static_cast<double>(total))
+                      << "%" << std::endl;
+        }
+
+
     } else {
         emnist.fitWork(rank);
         MPI_Finalize();
@@ -33,4 +75,5 @@ int main(int argc, char** argv) {
     }
 
     MPI_Finalize();
+    return 0;
 }
